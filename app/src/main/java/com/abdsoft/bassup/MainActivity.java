255bbbecec61fd06386adafe1e66a79037e4971c
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-09-01 13:05:00 +0530

    Initial commit - Create UI

---
+++ app/src/main/java/com/abdsoft/bassup/MainActivity.java
@@ -0,0 +1,15 @@
+package com.abdsoft.bassup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
